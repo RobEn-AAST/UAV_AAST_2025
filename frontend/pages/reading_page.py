9d28cb89f2cbe595b1fa9263b886e1a26957c7cb
Mohamed El-Razzaz <mohamedkarimkhairy@gmail.com>
2025-04-20 00:18:32 +0200

    Finished PDF reader logic.

--- frontend/pages/reading_page.py
+++ frontend/pages/reading_page.py
@@ -1,15 +1,21 @@
 from PyQt6.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QFileDialog, 
                             QLabel, QLineEdit, QGridLayout, QGroupBox,
-                            QTableWidget, QHBoxLayout)
+                            QTableWidget, QHBoxLayout, QTableWidgetItem)
 from PyQt6.QtCore import Qt
 from utils.file_handlers import FileHandler
+from utils.pdf_reader import convert_pdf
+from csv import reader
+import os
 
-# todo read csv to csv option as well, along with the pdf optioni to table to csv, ensure to save outputs
+# todo read csv to csv option as well, along with the pdf option to table to csv, ensure to save outputs
 class ReadingPage(QWidget):
     def __init__(self):
         super().__init__()
         self.file_handler = FileHandler()
         self.init_ui()
+
+        global current_self
+        current_self = self
 
     def init_ui(self):
         layout = QVBoxLayout(self)
@@ -51,7 +57,58 @@ class ReadingPage(QWidget):
         upload_button.clicked.connect(lambda: self.browse_file(selected_file_label))
 
     def browse_file(self, label):
-        filename, _ = QFileDialog.getOpenFileName(self, "Select PDF File", "", "PDF Files (*.pdf)")
-        if filename:
-            label.setText(filename)
-            # todo continue rest of pdf reading logic
+        #print(self.tables[1])
+        global file_name, file_type
+        file_name, file_type = QFileDialog.getOpenFileName(self, "Select PDF File", "", "PDF Files (*.pdf)")
+        if file_name:
+            #label.setText(file_name)
+            clicker()
+
+def clicker():
+    #file_name, file_type = QFileDialog.getOpenFileName(None, "Open File", "", "PDF(*.pdf)")
+    if file_name:
+        #current_self.selected_file_label.setText(file_name)
+        if file_name.endswith(".pdf"):
+            convert_pdf(file_name)
+            for i in range(1, 5):
+                #list_to_table(eval("current_self.csv_table_" + str(i)), i-1)
+                list_to_table(current_self.tables[i-1], i-1)
+        else:
+            #current_self.selected_file_label.setText("Please choose a PDF.")
+            pass
+
+def list_to_table(table, num):
+        
+        coord_list = csv_to_list()
+
+        for i, coord in enumerate(coord_list[num]):
+            lat_coord = QTableWidgetItem(coord[0])
+            lon_coord = QTableWidgetItem(coord[1])
+            alt_coord = QTableWidgetItem(coord[2])
+
+            table.setItem(i, 0, lat_coord)
+            table.setItem(i, 1, lon_coord)
+            table.setItem(i, 2, alt_coord)
+
+def csv_to_list():
+
+    coord_list_list = []
+    filepath = (__file__.replace("pages", "utils\\Output\\")).replace(os.path.basename(__file__), '')
+    dirlist = os.listdir(filepath)
+
+    for i in range(len(dirlist)-1):
+        if str(dirlist[i]).endswith(".csv") == False:
+            dirlist.pop(i)
+
+    for i in dirlist:
+        with open((filepath + i), 'r') as csvfile:
+            datareader = reader(csvfile)
+            coord_list = []
+            for row in datareader:
+                coord_sublist = []
+                for column in row:
+                    coord_sublist.append(column)
+                coord_list.append(coord_sublist)
+            coord_list_list.append(coord_list)
+
+    return coord_list_list
